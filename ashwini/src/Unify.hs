9d23d2b2acd3af300478e718f20030a16ef94ef0
Balaji Rao <balajirrao@gmail.com>
2020-02-03 23:06:24 +0100

    fix bind

--- ashwini/src/Unify.hs
+++ ashwini/src/Unify.hs
@@ -1,16 +1,12 @@
 module Unify where
 
-import Prelude hiding (lookup)
-
-import Data.Map (Map, lookup, insert)
-
-import Polysemy
-import Polysemy.State
-import Polysemy.Error
-
-import Type
-
+import Data.Map (Map, insert, lookup)
 import Errors
+import Polysemy
+import Polysemy.Error
+import Polysemy.State
+import Type
+import Prelude hiding (lookup)
 
 type Bindings = Map TypeVar Type
 
@@ -27,22 +23,20 @@ unify :: Members [State Bindings, Error CompilerError] r => Type -> Type -> Sem 
 unify l@(TyVar _) r = do
   rl <- resolve l
   rr <- resolve r
-
   case rl of
     Right tvar -> bind tvar r
     Left t -> case rr of
       Right tvar -> bind tvar l
       Left t' -> unify t t'
-
 unify l r@(TyVar _) = unify r l
-unify (TyFn a b) (TyFn c d) = unify a c >> unify b d
-unify (TyBase t1) (TyBase t2) | t1 == t2 = return ()
-                              | otherwise = throw UnificationError
+unify (TyFn a b) (TyFn c d) = unify b d >> unify a c
+unify (TyBase t1) (TyBase t2)
+  | t1 == t2 = return ()
+  | otherwise = throw UnificationError
 unify _ _ = throw UnificationError
 
 bind :: Member (State Bindings) r => TypeVar -> Type -> Sem r ()
-bind tv t@(TyVar tv') | tv == tv' = return ()
-                      | otherwise = do
-                          bindings <- get
-                          put (insert tv t bindings)
-bind _ _ = return ()
+bind tv (TyVar tv') | tv == tv' = return ()
+bind tv t = do
+  bindings <- get
+  put (insert tv t bindings)
